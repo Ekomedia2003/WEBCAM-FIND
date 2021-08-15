164cf046f423047082ea4539d2a4be5f07aba71f
Ekomedia2003 <88983251+Ekomedia2003@users.noreply.github.com>
2021-08-15 22:25:36 +0400

    banner.sh

--- files/banner.sh
+++ files/banner.sh
@@ -3,95 +3,54 @@
 #################### GÜNLER ####################
 
 pazartesi=$(date |grep -o Mon)
-
 sali=$(date |grep -o Tue)
-
 carsamba=$(date |grep -o Wed)
-
 persembe=$(date |grep -o Thu)
-
 cuma=$(date |grep -o Fri)
-
 cumartesi=$(date |grep -o Sat)
-
 pazar=$(date |grep -o Sun)
 
 #################### GÜNLERE GÖRE RENKLER ####################
 
 if [[ $pazartesi == Mon ]];then
-
-	color='\e[32m'elif [[ $sali == Tue ]];then
-
-	color='\e[33m'
-
-elif [[ $carsamba == Wed ]];then
-
-	color='\e[34m'
-
-elif [[ $persembe == Thu ]];then
-
-	color='\e[31m'
-
-elif [[ $cuma == Fri ]];then
-
-	color='\e[36m'
-
-elif [[ $cumartesi == Sat ]];then
-
 	color='\e[32m'
-
-elif [[ $pazar == Sun ]];then
-
+elif [[ $sali == Tue ]];then
 	color='\e[33m'
-
+elif [[ $carsamba == Wed ]];then
+	color='\e[34m'
+elif [[ $persembe == Thu ]];then
+	color='\e[31m'
+elif [[ $cuma == Fri ]];then
+	color='\e[36m'
+elif [[ $cumartesi == Sat ]];then
+	color='\e[32m'
+elif [[ $pazar == Sun ]];then
+	color='\e[33m'
 fi
 
 #################### BANNER ####################
 
 printf "
-
 \e[97m
-
 　 　       ▒█░░▒█ ▒█▀▀▀ ▒█▀▀█ ▒█▀▀█ ░█▀▀█ ▒█▀▄▀█ 
-
 　 　       ▒█▒█▒█ ▒█▀▀▀ ▒█▀▀▄ ▒█░░░ ▒█▄▄█ ▒█▒█▒█ 
-
 　 　       ▒█▄▀▄█ ▒█▄▄▄ ▒█▄▄█ ▒█▄▄█ ▒█░▒█ ▒█░░▒█ 
-
 $color
-
 　 　 　              ▒█▀▀▀ ▀█▀ ▒█▄░▒█ ▒█▀▀▄ 
-
 　 　 　              ▒█▀▀▀ ▒█░ ▒█▒█▒█ ▒█░▒█ 
-
 　 　 　              ▒█░░░ ▄█▄ ▒█░░▀█ ▒█▄▄▀ 
-
                          \e[33mSON GÜNCELLEME :\e[0m $(cd .. && sed -n 3p README.md |tr -d "Güncelleme ")
-
          \e[1;97m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-
-              $color C O D E D   B Y  EKO CYBER SECURITY
-
+              $color C O D E D   B Y   EKO CYBER SECURITY
          \e[97m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-
 	 \e[97m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-
-	          $color T E R M U X - AZERBAYCAN
-
+	          $color INSTA : EKOMEDIA_SMM
   	 \e[0;97m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-
 	     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-
 	         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-
 		     +-+-+-+-+-+-+-+-+-+-+-+
-
 		         +-+-+-+-+-+-+-+
-
 			     +-+-+-+
-
 			       +-+
-
 			        +
-
 "
